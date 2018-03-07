f20b453048f227d0b9b4c3dfedc7a432c3456a6a
diego-c <diechaves@gmail.com>
2018-03-07 19:02:11 -0300

    rebuild docs

--- utils/checkFilename.js
+++ utils/checkFilename.js
@@ -3,7 +3,7 @@
  * If it doesn't, tries to guess the extension from the provided URL and appends it to the filename
  * @param { string } filename The filename to be checked
  * @param { string } url The URL to guess the file extension
- * @returns { string } Returns either the original filename (if it already contains a file extension) or the filename.extension
+ * @returns { string } Returns either the original filename (if it already contains a file extension) or the filename._extension_
  */
 
 const checkFilename = (filename, url) => {
